5a44593279b59b406d004aebdf729a9f222f0d88
Ramees Jahan <rameesjahan37@gmail.com>
2023-12-31 17:11:37 +0530

    Added Dark Theme

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 //Tailwind Configuration
 tailwind.config = {
+      darkMode:"class",
       theme: {
         extend: {
           colors: {
@@ -8,6 +9,26 @@ tailwind.config = {
         }
       }
     }
+
+//Toggle Dark Mode
+const loadTheme = () => {
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+    document.getElementById('icon_dark').src = '../icons/sun.svg'
+  } else {
+    document.documentElement.classList.remove('dark')
+    document.getElementById('icon_dark').src = '../icons/moon.svg'
+  }
+}
+
+const toggleDark = () => {
+  if(localStorage.theme === 'light'){
+    localStorage.theme = 'dark'
+  }else{
+    localStorage.theme = 'light'
+  }
+  loadTheme()
+}
 
 
 
@@ -19,7 +40,7 @@ const loadInput = () => {
   sgpa_list.forEach((item,i) => {
     if(i>0){ 
       //if sgpa list has morethan 1 item add delete button 
-      data +=`<div class="flex border rounded mt-1.5 overflow-hidden">
+      data +=`<div class="flex border rounded mt-1.5 overflow-hidden bg-white">
                 <input class="w-full p-1.5 outline-none" type="number" name="sgpa_${i+1}" id="sgpa_${i+1}" placeholder="Enter SGPA for Sem ${i+1}" onchange="updateInput(this,${i})" value="${item}"/>
                 <img class="w-[24px] m-1.5 p-0.5 shrink" src="icons/trash.svg" alt="Delete" onclick="handleDel(${i})" />
               </div>`
@@ -49,6 +70,7 @@ const updateInput = (target,i) => {
 }
 
 window.onload = () => {
+  loadTheme()
   loadInput()
 }
 
@@ -59,6 +81,8 @@ const handleSubmit = () => {
     total_sgpa = (sum/(sem*10))*100
     document.getElementById('sgpa').innerText=Number((total_sgpa).toFixed(2))+"%"
   }
+  sgpa_list=['']
+  loadInput()
 }
 
 function getSem(arr) {
@@ -68,6 +92,7 @@ function getSem(arr) {
   })
   return s
 }
+
 function getSum(arr) {
   let s=0
   arr.forEach((item) => {
